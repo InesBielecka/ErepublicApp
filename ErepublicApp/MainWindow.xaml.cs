f3558a9e717946beb9c15d1681ed97a87da1daee
ines <ines.bielecka@gmail.com>
2018-01-23 15:02:52 +0100

    Modeling json file into c#

--- ErepublicApp/MainWindow.xaml.cs
+++ ErepublicApp/MainWindow.xaml.cs
@@ -14,18 +14,66 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Web.Script.Serialization;
 using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using ErepublicApp.Models;
 
 namespace ErepublicApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
 
             InitializeComponent();
+        }
+
+
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.erepublik.com/en/military/campaigns-new");
+            //client.DefaultRequestHeaders.Add("appkey", "myapp_key");
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            HttpResponseMessage response = client.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var campaignsString = response.Content.ReadAsStringAsync().Result; // ReadAsA sync<IEnumerable<Employee>>().Result;
+
+                var objects = JObject.Parse(campaignsString); // parse as array  
+                var campaings = objects.ToObject<Campaigns>();
+                //var battles = objects["battles"];
+                foreach(var bitwa in campaings.Battles)
+                {
+                    tb1_battleId.Text = bitwa.Value.Id.ToString();
+                    textBox.Text = bitwa.Value.Div.Second.Div.ToString()??"";
+                }
+                //var battlesModel = battles.ToObject<List<Battle>>();
+
+                //textBox.Text = battlesModel.First().War_id.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void tb1_battleId_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
---
+++ ErepublicApp/Models/Battle.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErepublicApp.Models
+{
+    class Battle
+    {
+        public int Id { get; set; }
+        public int War_id { get; set; }
+        public int Zone_id { get; set; }
+        public bool Is_rw { get; set; }
+        public bool Is_as { get; set; }
+        public string Type { get; set; }
+        public int Start { get; set; }
+        public int Det { get; set; }
+        public Region Region { get; set; }
+        public bool Is_dict { get; set; }
+        public bool Is_lib { get; set; }
+        [JsonProperty("inv")]
+        public BattleSite Invader { get; set; }
+        [JsonProperty("def")]
+        public BattleSite Defender { get; set; }
+        public Division Div { get; set; }
+
+    }
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BattleSite
+    {
+        public int Id { get; set; }
+        public int[] Allies { get; set; }
+        public AllyList[] Ally_list { get; set; }
+        public int Points { get; set; }
+    }
+
+    public class AllyList
+    {
+        public int Id { get; set; }
+        public bool Deployed { get; set; }
+    }
+
+    public class Division
+    {
+        [JsonProperty("1")]
+        public DivisionNumber First { get; set; }
+        [JsonProperty("2")]
+        public DivisionNumber Second { get; set; }
+        [JsonProperty("3")]
+        public DivisionNumber Third { get; set; }
+        [JsonProperty("4")]
+        public DivisionNumber Fourth { get; set; }
+    }
+
+    public class DivisionNumber
+    {
+        public int Div { get; set; }
+        public bool? End { get; set; }
+        public int Epic { get; set; }
+        public int Epic_type { get; set; }
+
+    }
+}
