4a7da768609c583a419145f2c2d66de6814af8af
ines <ines.bielecka@gmail.com>
2018-02-13 10:32:51 +0100

    making template

--- ErepublicApp/MainWindow.xaml.cs
+++ ErepublicApp/MainWindow.xaml.cs
@@ -27,36 +27,29 @@ namespace ErepublicApp
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     /// 
-    public class TabTemplate
-    {
-        public object Template { get; private set; }
-
-        public TabTemplate(object content)
-        {
-            Template = content;
-        }
-    }
-
 
     public partial class MainWindow : Window
     {
-        private TabTemplate template;
+        int _battleId;
+        HttpResponseMessage _response;
+        HttpClient _client;
+        private Campaigns _campaings;
+        private System.Timers.Timer _timer;
+        List<int> _currentBattles;
+
         public MainWindow()
         {
-
             InitializeComponent();
-            template = new TabTemplate(tab_battle_id.Content);
+            _currentBattles = new List<int>(_battleId);
         }
 
 
         private void tabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
         private void txtbx_battle_id_TextChanged(object sender, TextChangedEventArgs e)
         {
-           
         }
 
         private void txtbx_battle_id_GotFocus(object sender, RoutedEventArgs e)
@@ -69,92 +62,61 @@ namespace ErepublicApp
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
         }
-        
 
         private void btn_show_battle_Click(object sender, RoutedEventArgs e)
         {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("https://www.erepublik.com/en/military/campaigns-new");
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _response = _client.GetAsync("").Result;
 
-            HttpResponseMessage response = client.GetAsync("").Result;
-
-            if (response.IsSuccessStatusCode)
+            if (_response.IsSuccessStatusCode)
             {
-                var campaignsString = response.Content.ReadAsStringAsync().Result; // ReadAsA sync<IEnumerable<Employee>>().Result;
+                var campaignsString = _response.Content.ReadAsStringAsync().Result; // ReadAsA sync<IEnumerable<Employee>>().Result;
 
                 var objects = JObject.Parse(campaignsString); // parse as array  
-                var campaings = objects.ToObject<Campaigns>();
+                _campaings = objects.ToObject<Campaigns>();
 
-                int battleId = Int32.Parse(txtbx_battle_id.Text);
-                var chosenBattle = campaings.Battles.Values.Where(x => x.Id == battleId);
+                _battleId = Int32.Parse(txtbx_battle_id.Text);
+                _currentBattles.Add(_battleId);
+                var chosenBattle = _campaings.Battles.Values.Where(x => x.Id == _battleId);
+                var battlesList = _campaings.Battles.Values.Where(x => _currentBattles.Contains(x.Id)).ToList();
 
+                tabControlBattles.ItemsSource = _currentBattles;
 
-                if(tabControlBattles.Items.Count > 1)
-                {
-                    template = new TabTemplate(tab_battle_id.Content);
-                    var tabItemNew = new TabItem { Header = battleId.ToString(), Content = template.Template };
-                    var tabItemNewIndex = tabControlBattles.Items.Add(tabItemNew);
-                }
-                else
-                {
-                    var tabItemNew = new TabItem { Header = battleId.ToString(), Content = template.Template };
-                    var tabItemNewIndex = tabControlBattles.Items.Add(tabItemNew);
-                }
-
-                    foreach (var batl in chosenBattle)
-                    {
-                        lbl_type_show.Content = batl.Type.ToString();
-                        lbl_region_show.Content = batl.Region.Name.ToString();
-                        int countryInvId = batl.Invader.Id;
-                        lbl_invader_country_name.Content = Constants.countriesDic[countryInvId];
-                        int countryDefId = batl.Defender.Id;
-                        lbl_defender_country_name.Content = Constants.countriesDic[countryDefId];
-
-                        if (batl.Type.ToString() == "tanks")
-                        {
-                            EnableTabs(true);
-
-                            SetWall(batl, "1", lbl_wall_for_div1, lbl_wall_dom_div1);
-                            SetWall(batl, "2", lbl_wall_for_div2, lbl_wall_dom_div2);
-                            SetWall(batl, "4", lbl_wall_for_div3, lbl_wall_dom_div3);
-                            SetWall(batl, "3", lbl_wall_for_div4, lbl_wall_dom_div4);
-                        }
-                        else
-                        {
-                            EnableTabs(false);
-                            SetWall(batl, "11", lbl_wall_for_air, lbl_wall_dom_air);
-                        }
-                    }
-                
-
-
-                //var polskieWalki = campaings.Battles.Values.Where(x => x.Invader.Id == 35 || x.Defender.Id == 35);
-                //var liczbaPolskichWalk = polskieWalki.Count();
-                //var typWalki = polskieWalki.Where(x => x.Type == Constants.Tanks).Count();
-
-                //foreach(var walka in polskieWalki)
-                //{
-                //   foreach(var dywizja in walka.Div)
-                //    {
-                //        textBlock.Text += dywizja.Value.Wall.For + ": " + dywizja.Value.Wall.Dom.ToString() + Environment.NewLine;
-                //    }
-                //}
-
-                //foreach (var bitwa in campaings.Battles.Reverse())
-                //{
-                //    tb1_battleId.Text = bitwa.Value.Id.ToString();
-                //    textBox.Text = typWalki.ToString();
-                //}
-                //var battlesModel = battles.ToObject<List<Battle>>();
-
-                //textBox.Text = battlesModel.First().War_id.ToString();
+                var tabItemNew = new TabItem { Header = _battleId.ToString()};
+                var tabItemNewIndex = tabControlBattles.Items.Add(tabItemNew);
+                tabItemNew.Focus();
             }
             else
             {
-                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+                MessageBox.Show("Error Code" + _response.StatusCode + " : Message - " + _response.ReasonPhrase);
             }
+        }
 
+        private void FillingData(IEnumerable<Battle> chosenBattle)
+        {
+            foreach (var batl in chosenBattle)
+            {
+                lbl_type_show.Content = batl.Type.ToString();
+                lbl_region_show.Content = batl.Region.Name.ToString();
+                int countryinvid = batl.Invader.Id;
+                lbl_invader_country_name.Content = Constants.countriesDic[countryinvid];
+                int countrydefid = batl.Defender.Id;
+                lbl_defender_country_name.Content = Constants.countriesDic[countrydefid];
+
+                if (batl.Type.ToString() == "tanks")
+                {
+                    EnableTabs(true);
+
+                    SetWall(batl, "1", lbl_wall_for_div1, lbl_wall_dom_div1, progressbardiv1);
+                    SetWall(batl, "2", lbl_wall_for_div2, lbl_wall_dom_div2, progressbardiv2);
+                    SetWall(batl, "4", lbl_wall_for_div3, lbl_wall_dom_div3, progressbardiv3);
+                    SetWall(batl, "3", lbl_wall_for_div4, lbl_wall_dom_div4, progressbardiv4);
+                }
+                else
+                {
+                    EnableTabs(false);
+                    SetWall(batl, "11", lbl_wall_for_air, lbl_wall_dom_air, progressbarair);
+                }
+            }
         }
 
         private void EnableTabs(bool IsTankBattle)
@@ -164,7 +126,7 @@ namespace ErepublicApp
             tab_div2.IsEnabled = IsTankBattle;
             tab_div3.IsEnabled = IsTankBattle;
             tab_div4.IsEnabled = IsTankBattle;
-            if(IsTankBattle)
+            if (IsTankBattle)
             {
                 tab_div4.Focus();
             }
@@ -174,11 +136,41 @@ namespace ErepublicApp
             }
         }
 
-        private void SetWall(Battle batl, string divNumber, Label lbl_wall_for, Label lbl_wall_dom)
+        private void SetWall(Battle batl, string divNumber, Label lbl_wall_for, Label lbl_wall_dom, ProgressBar progressbardiv)
         {
             int countryWallForId = batl.Div[divNumber].Wall.For;
             lbl_wall_for.Content = Constants.countriesDic[countryWallForId];
             lbl_wall_dom.Content = batl.Div[divNumber].Wall.Dom.ToString();
+            progressbardiv.Value = Double.Parse(batl.Div[divNumber].Wall.Dom.ToString());
+        }
+
+        private void tabControlBattles_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int k = tabControlBattles.SelectedIndex;
+
+            TabItem selectedTab = e.AddedItems[0] as TabItem;
+
+            if (int.TryParse(selectedTab.Header.ToString(), out _battleId))
+            {
+                var chosenBattle = _campaings.Battles.Values.Where(x => x.Id == _battleId);
+                //FillingData(chosenBattle);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://www.erepublik.com/en/military/campaigns-new");
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            _timer = new System.Timers.Timer();
+            _timer.Interval = 5000;
+            _timer.Elapsed += _timer_Elapsed;
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            _response = _client.GetAsync("").Result;
         }
     }
 }
